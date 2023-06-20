e42ea5ab629d9c8fda07984bf7f3b5f7bff0e307
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-06-20 23:32:39 +0500

    feat(employee portal): update

--- employee/views/sidebar.php
+++ employee/views/sidebar.php
@@ -4,37 +4,11 @@
           <ul>
             <div class="logo">
               <a href="./">
-                <span>Admin-Easy Home</span>
+                <span>Employee-Easy Home</span>
               </a>
             </div>
-            <li><a class="sidebar-sub-toggle"><i class="ti-bag"></i> Employees <span
-                  class="sidebar-collapse-icon ti-angle-down"></span></a>
-              <ul>
-                <li><a href="./employees.php">View All</a></li>
-                <li><a href="./addEmployee.php">Add Employee</a></li>
-                <li><a href="./editEmployee.php">Edit Employee</a></li>
-              </ul>
-            </li>
-            <li><a class="sidebar-sub-toggle"><i class="ti-shopping-cart-full"></i> Orders <span
-                  class="sidebar-collapse-icon ti-angle-down"></span></a>
-              <ul>
-                <li><a href="./orders.php">View All</a></li>
-                <li><a href="./pendingOrders.php?status=pending">Pending Orders</a></li>
-                <li><a href="./orders.php?status=completed">Completed Orders</a></li>
-                <li><a href="./orders.php?status=cancelled">Cancelled Orders</a></li>
-              </ul>
-            </li>
-            <li><a class="sidebar-sub-toggle"><i class="ti-panel"></i> Services <span
-                  class="sidebar-collapse-icon ti-angle-down"></span></a>
-              <ul>
-                <li><a href="./services.php">View All</a></li>
-                <li><a href="./addService.php">Add Service</a></li>
-                <li><a href="./editService.php">Edit Service</a></li>
-              </ul>
-            </li>
-            <li><a href="./users.php"><i class="ti-user"></i> Users</a></li>
-            <li><a href="./reports.php"><i class="ti-stats-up"></i> Reports</a></li>
-            <li><a href="./myProfile.php"><i class="ti-id-badge"></i> My Profile</a></li>
+            <li><a href="./users.php"><i class="ti-user"></i> Requests</a></li>
+            <li><a href="./reports.php"><i class="ti-stats-up"></i> Accepted Requests</a></li>
             <li><a href="./logout.php"><i class="ti-close"></i> Logout</a></li>
           </ul>
         </div>
