4b2f386cdd085fbc50ff3f35ca7a9e112a70bb9a
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-06-26 17:38:49 +0500

    feat(client forgot password): page ui ready

--- forgotPassword.php
+++ forgotPassword.php
@@ -22,18 +22,21 @@
 
   <br /><br />
 
-  <div class="forgot-password-page-container">
+  <div id="forgot-password-page-container">
     <h2>Forgot Password</h2>
 
-    <form action="./process/process_forgot_password.php" method="POST" id="forgot-password-form">
-      <div class="form-group" id="forgot-password-email-input-wrapper">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" placeholder="Enter your email" name="email" maxlength="255" required />
-      </div>
-      <div id="forgot-password-form-reset-btn-wrapper">
-        <input type="submit" class="form-control btn btn-primary" value="Reset" />
-      </div>
-    </form>
+    <div id="forgot-password-page-content-wrapper">
+      <form action="./process/process_forgot_password.php" method="POST" id="forgot-password-form">
+        <div class="form-group" id="forgot-password-email-input-wrapper">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" placeholder="Enter your email" name="email" maxlength="255"
+            required />
+        </div>
+        <div id="forgot-password-form-reset-btn-wrapper">
+          <input type="submit" class="form-control btn btn-primary" value="Reset" />
+        </div>
+      </form>
+    </div>
   </div>
 
   <br /><br /><br />
